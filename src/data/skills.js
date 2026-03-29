7f3b506119bc3ab8b0ff220d360f9d6c69971818
Pradeep Kumar Singha <mr.pradeepkumarsingha@gmail.com>
2026-03-29 21:26:19 +0530

    updated skill

--- src/data/skills.js
+++ src/data/skills.js
@@ -8,13 +8,21 @@ export const skills = {
     { name: "Tailwind CSS", level: 85, icon: SiTailwindcss, color: "#06B6D4" },
     { name: "Node.js", level: 75, icon: FaNodeJs, color: "#339933" },
     { name: "Express", level: 70, icon: SiExpress, color: "#ffffff" },
-    { name: "MongoDB", level: 75, icon: SiMongodb, color: "#47A248" }
+    { name: "MongoDB", level: 75, icon: SiMongodb, color: "#47A248" },
+    { name: "RESTful APIs", level: 80, icon: FaNodeJs, color: "#339933" },
+    { name: "MySQL", level: 70, icon: SiMongodb ,color: "#339933" }
+
   ],
   ai: [
     { name: "Python", level: 80, icon: FaPython, color: "#3776AB" },
     { name: "Scikit-Learn", level: 75, icon: SiScikitlearn, color: "#F7931E" },
     { name: "TensorFlow", level: 70, icon: SiTensorflow, color: "#FF6F00" },
     { name: "Pandas", level: 80, icon: FaPython, color: "#150458" },
-    { name: "NumPy", level: 78, icon: FaPython, color: "#013243" }
+    { name: "NumPy", level: 78, icon: FaPython, color: "#013243" },
+    { name: "Keras", level: 70, icon: SiTensorflow, color: "#D00000" },
+    { name: "Computer Vision", level: 75, icon: SiTensorflow, color: "#FF6F00" },
+    { name: "Natural Language Processing", level: 80, icon: SiScikitlearn, color: "#F7931E" }
+    {name:"GEN AI", level: 80, icon: SiTensorflow, color: "#FF6F00"}
+
   ]
 };
